14fab88bedae334af2144f373018f881677ab5f6
Ionésio Junior <ionesiojr@gmail.com>
2024-12-13 14:32:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import json
 if __name__ == "__main__":
     client = Client.load()
 
-    ionesio_datasite_path = client.datasites / "user@email.com" / "api_data" / "no_spoiler" / (client.email + ".json")
+    output_path = client.datasites / "user@email.com" / "api_data" / "no_spoiler" / (client.email + ".json")
     dataset = load_dataset("Netflix Data")
     
     result = "No"
     if "Movie/Serie Title" in dataset['Title']:
        result = "Yes"
 
-    with open(str(ionesio_datasite_path), 'w') as json_file:
+    with open(str(output_path), 'w') as json_file:
         json.dump({"watched": result},json_file, indent=4)
 
